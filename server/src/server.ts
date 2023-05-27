8e71030ea3cb36cbe0303d2082705db2e3d631c0
diego64 <diegoferreira1964@gmail.com>
2023-05-27 17:26:04 -0300

    Configurando a rota de upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,22 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+
+import { resolve } from 'node:path'
+
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -17,6 +29,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
