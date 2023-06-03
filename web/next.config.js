d85258bcd8586703ec870cd4dd8e859c8ac07caf
diego64 <diegoferreira1964@gmail.com>
2023-06-03 17:32:52 -0300

    Finalizaçao da aplicacao

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.0.25',
+        port: '3333',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 }
 
